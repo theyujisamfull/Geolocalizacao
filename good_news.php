bad07c931d2d97d1276687d3a49ff7c229e4adb0
Victor Yuji <theyujisamfulll@gmail.com>
2019-01-26 00:32:50 -0200

    teste

---
+++ good_news.php
@@ -0,0 +1,89 @@
+<?php include("../conexao.php"); ?>
+<?php date_default_timezone_set('America/Sao_Paulo');?>
+ 
+<html>
+  <head>
+  	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Good News</title><meta charset="utf-8">
+	  <style> 
+		#table{position:absolute; top:100px; left:200px;}
+		h5{width:400px;}
+		.cor{background-color:rgb(203, 179, 255);}
+		body{background-color:rgb(255, 119, 51);}
+	  </style>
+  </head>
+	<body>
+
+	
+	
+	<div class='container'>
+		<div class='row'>
+
+		<div class='col-md-3'>		</div>
+		<div class='col-md-9'>
+			<a href='https://globoplay.globo.com/v/2321390/' ><h3>Globo - Postura, Força de crescer </h3> </a>
+		</div>
+		
+		
+		<div class='col-md-3'>
+			
+		</div>
+		<div class='col-md-9'>
+			<a href='https://jobs.kenoby.com/bancovotorantim/job/estagio/5b84618cad2ef32916be4ab1'><h3> Estágio - Banco Votorantim </h3> </a>
+			<p class='text-justify'> Estamos em busca de estudantes que queiram aprender e se desenvolver em um ambiente simples e ágil, além de trabalhar em equipes que as relações são informais, que valorizam a inovação e atuam de forma colaborativa.
+<br><br>Os estudantes terão oportunidade de estagiar nas diversas frentes que temos no Banco (Atacado, Varejo e áreas Corporativas), entre elas: Comercial Varejo, <strong>Inovação Digital</strong>, BV Lab, Produtos, Tesouraria,<strong> Investment Banking, Asset Management, Private Banking, Corporate</strong>, Auditoria, Controles, Governança, Operações, Finanças, RH, Jurídico, Marketing, Sustentabilidade, <strong>Tecnologia, Riscos, Ciências de Dados e Crédito</strong>.
+<br><br>Gostou? Então veja se você tem o perfil que buscamos:
+<br>Formação superior cursando com conclusão prevista entre jul/19 e dez/20;
+<br>Bons conhecimentos em Pacote Office (principalmente Excel);
+<br>Inglês a partir de Intermediário;
+<br>Disponibilidade para estagiar 30 horas semanais;
+<br>Nossas vagas de estágio são abertas ao longo do ano, por isso, fique ligado! A qualquer momento podemos te contatar!
+<br>Aqui no Banco, além dos benefícios de mercado, trabalhamos em um ambiente moderno com estações colaborativas, temos uma jornada flexível, parceria com a Gympass e o nosso dress code é casual de segunda a sexta-feira ; )
+<br>Agora é só se inscrever e ficar na torcida \o/</p>
+		</div>			
+		
+		<div class='col-md-9'>
+		<h2>Quais o pontos que preciso melhorar para me candidatar</h2>
+		</div>
+		
+		
+		<div class='col-md-3'>
+		</div>
+		
+		<div class='col-md-3'>
+		
+		
+
+		</div>
+		
+		<div class='col-md-9'>
+			<a href='https://www.cursosdeexcelonline.com.br/blog/dicas/excel-avancado-voce-sabe-realmente-o-que-e'><h3> O que é considerado Excel avançado? </h3> </a>
+			<ul> 
+			<li>Criar gráficos </li> 
+			<li>Atalhos do teclado </li> 
+			<li>Utilizar opções da guia </li> 
+			<li>Utilizar funções built-in </li> 
+			<li>Fazer tabelas dinâmicas </li> 
+			<li>Utilizar função Solver </li> 
+			<li>Criação de relatórios e Dashboards	 </li> 
+			<li>Utilizar filtros</li> 
+			</ul>
+		</div>			
+
+
+		<div class='col-md-3'>
+				<iframe width="260" height="115" src="https://www.youtube.com/embed/tKRBRuCZkpA?start=65" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+			</div>
+			<div class='col-md-4'>
+				<a href='https://www.youtube.com/watch?v=tKRBRuCZkpA'><h3> Por que não se pode simplesmente imprimir dinheiro?</h3> </a>
+				<h5> Suponha que se imprima dinheiro, o poder de compra das pessoas iria aumentar. Então as pessoas iriam gastar mais </h5>
+			</div>
+			
+
+			
+		</div>
+	</div>
+	
+	</body>
+	
+</html>
